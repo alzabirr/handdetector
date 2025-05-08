aa464b3472f0bfaeb98ece16a7c6484134baa16b
alzabirr <alzabirhussain@gmail.com>
2025-05-09 01:57:58 +0600

    ok

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+from cvzone.HandTrackingModule import  HandDetector
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+detector = HandDetector (detectionCon=0.6,maxHands=2)
+
+while True:
+    _,img = cap.read()
+    hands, img = detector.findHands(img)
+    cv2.imshow("smart Camera",img)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+from pycparser.ply.yacc import resultlimit
+
+
